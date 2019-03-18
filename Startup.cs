496ddcec87936667b481b67532f9aeab2ad92304
Mike Silva <misilva@passadm.local>
2019-03-18 17:14:57 -0300

    first commit

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(options =>
+                        {
+                            options.SwaggerDoc("v1",
+                                new Info
+                                {
+                                    Title = "Teste Api Mike",
+                                    Version = "v1",
+                                    Description = "Projeto de Teste ASP.Net Core",
+                                    Contact = new Contact
+                                    {
+                                        Name = "Mike Teste",
+                                        Url = ""
+                                    }
+                                });
+                        });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.RoutePrefix = "swagger";
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo JWT Api");
+                });
+
+
+
+            app.UseMvc();
+        }
+    }
+}
